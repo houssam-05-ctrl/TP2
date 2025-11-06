224fbbd97fcc1cad37bca90d173ee55cef205f5f
Houssam <elbakkourihoussam4@gmail.com>
2025-11-06 01:20:47 +0100

    calculator

---
+++ calculator/calculator.js
@@ -0,0 +1,103 @@
+const buttonValues = [
+    "AC", "+/-", "%", "÷", 
+    "7", "8", "9", "×",
+    "4", "5", "6", "-",
+    "1", "2", "3", "+",
+    "0", ".", "="
+];
+const rightSymbols = ["÷", "×", "-", "+", "="];
+const topSymbols = ["AC", "+/-", "%"];
+
+const display = document.getElementById("display");
+
+let A=0;
+let B=null ;
+let operator=null ;
+
+function clearall(){
+     A=0;
+     B=null ;
+     operator=null ;
+}
+
+for(let i =0; i<buttonValues.length; i++){
+    let value = buttonValues[i];
+    let button = document.createElement("button"); // create un nouvel élément bouton
+    button.innerText = value ;
+
+    //styling the buttons color :
+    if(value == "0"){
+        button.style.width = "200px";
+        button.style.gridColumn ="span 2";
+    }
+    if (rightSymbols.includes(value)){
+        button.style.backgroundColor = "#FF9500";
+    }
+    else if (topSymbols.includes(value)){
+        button.style.backgroundColor="#D4D4D2";
+        button.style.color ="#1C1C1C";
+    }
+
+    //process button clicks 
+    button.addEventListener("click", function(){
+        if (rightSymbols.includes(value)){
+        if(value=="="){
+            if(A != null){
+                B = display.value ;
+                let numA = Number(A);
+                let numB = Number(B);
+
+                if(operator=="÷"){
+                     display.value = numA/numB;
+                }
+                else if (operator=="×"){
+                    display.value = numA*numB;
+                }
+                else if (operator=="+"){
+                    display.value = numA+numB;
+                }
+                else if (operator=="-"){
+                    display.value = numA - numB;
+                }
+                clearall();
+            }
+        }
+        else {
+            operator = value ;
+            A = display.value;
+            display.value = "";
+        }
+    }
+    else if (topSymbols.includes(value)){
+        if( value == "AC"){
+            clearall();
+            display.value = "";
+        }
+        else if ( value == "+/-"){
+                if(display.value != "" && display.value != "0"){
+                    if(display.value[0]=="-"){
+                        display.value = display.value.slice(1)
+                    }
+                }
+        }
+        else if ( value == "%"){
+            display.value = Number(display.value)/100; 
+        }
+    }
+    else {
+        if(value=="."){
+            if(display.value != "" && !display.value.includes(value)){// check if the user has put a".""
+                display.value += value ;
+            }
+        }
+        else if (display.value == "0"){ 
+            display.value = value ;
+        }
+        else {
+            display.value += value ;
+        }
+    }
+    });
+    // ajouter les boutons a la calculatrice
+    document.getElementById("buttons").appendChild(button);
+}
